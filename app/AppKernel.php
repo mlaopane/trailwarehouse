f0d8ddaacc4f80645896275569dd5e53bf1ba454
MykeAdmin <MykeAdmin>
2017-05-08 20:32:04 +0200

    Created 3 main bundles : TrailWarehouseAppBundle, TrailWarehouseCoreBundle, TrailWarehouseAdminBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
+            new TrailWarehouse\CoreBundle\TrailWarehouseCoreBundle(),
+            new TrailWarehouse\AppBundle\TrailWarehouseAppBundle(),
+            new TrailWarehouse\AdminBundle\TrailWarehouseAdminBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
