d09cbad96c08260432e55d9a060b5daf5fd32226
mlaopane <mlaopane@gmail.com>
2017-05-23 01:52:16 +0200

    Added some 'home routes' Added links inside the footer

--- src/TrailWarehouse/AppBundle/Controller/HomeController.php
+++ src/TrailWarehouse/AppBundle/Controller/HomeController.php
@@ -11,4 +11,22 @@ class HomeController extends Controller
         $data = [];
         return $this->render('TrailWarehouseAppBundle:Home:index.html.twig', $data);
     }
+
+    public function contactAction()
+    {
+        $data = [];
+        return $this->render('TrailWarehouseAppBundle:Home:contact.html.twig', $data);
+    }
+
+    public function aboutAction()
+    {
+        $data = [];
+        return $this->render('TrailWarehouseAppBundle:Home:about.html.twig', $data);
+    }
+
+    public function mentionsAction()
+    {
+        $data = [];
+        return $this->render('TrailWarehouseAppBundle:Home:mentions.html.twig', $data);
+    }
 }
