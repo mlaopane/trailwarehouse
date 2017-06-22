8860d6c46083253bf6a1cd3958fba2764c445d4b
Mickael MLP. LAO-PANE <mlao-pane@ciage.net>
2017-06-22 10:11:25 +0200

    Updated LoadColor fixture (not handling type = duo anymore)

--- src/TrailWarehouse/AppBundle/DataFixtures/ORM/LoadColor.php
+++ src/TrailWarehouse/AppBundle/DataFixtures/ORM/LoadColor.php
@@ -13,15 +13,13 @@ class LoadColor implements FixtureInterface
   public function load(ObjectManager $manager)
   {
     $data = [
-      ['name' => 'rouge', 'value'      => '#ec134f', 'type' => 'solo'],
-      ['name' => 'vert', 'value'       => '#26d93d', 'type' => 'solo'],
-      ['name' => 'bleu', 'value'       => '#2b8cee', 'type' => 'solo'],
-      ['name' => 'jaune', 'value'      => '#fde32d', 'type' => 'solo'],
-      ['name' => 'blanc', 'value'      => '#ffffff', 'type' => 'solo'],
-      ['name' => 'gris', 'value'       => '#cdcdcd', 'type' => 'solo'],
-      ['name' => 'noir', 'value'       => '#010101', 'type' => 'solo'],
-      ['name' => 'noir/gris', 'value'  => '#cdcdcd', 'type' => 'duo'],
-      ['name' => 'noir/rouge', 'value' => '#010101', 'type' => 'duo'],
+      ['name' => 'rouge', 'value' => '#ec134f', 'type' => 'solo'],
+      ['name' => 'vert', 'value'  => '#26d93d', 'type' => 'solo'],
+      ['name' => 'bleu', 'value'  => '#2b8cee', 'type' => 'solo'],
+      ['name' => 'jaune', 'value' => '#fde32d', 'type' => 'solo'],
+      ['name' => 'blanc', 'value' => '#ffffff', 'type' => 'solo'],
+      ['name' => 'gris', 'value'  => '#cdcdcd', 'type' => 'solo'],
+      ['name' => 'noir', 'value'  => '#010101', 'type' => 'solo'],
     ];
     foreach ($data as $element) {
       $item = (new Color())
