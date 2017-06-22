09eae12f955beaf6b15b98f7f6d495dac842e9d0
Mickael MLP. LAO-PANE <mlao-pane@ciage.net>
2017-06-22 10:20:24 +0200

    Removed sizes from LoadSize fixture

--- src/TrailWarehouse/AppBundle/DataFixtures/ORM/LoadSize.php
+++ src/TrailWarehouse/AppBundle/DataFixtures/ORM/LoadSize.php
@@ -13,24 +13,19 @@ class LoadSize implements FixtureInterface
   public function load(ObjectManager $manager)
   {
     $data = [
-      ['value' => 'U', 'unit' => NULL, 'unit_shortcut' => NULL],
+      ['value' => 'U', 'unit'  => NULL, 'unit_shortcut' => NULL],
       ['value' => 'XS', 'unit' => NULL, 'unit_shortcut' => NULL],
-      ['value' => 'S', 'unit' => NULL, 'unit_shortcut' => NULL],
-      ['value' => 'M', 'unit' => NULL, 'unit_shortcut' => NULL],
-      ['value' => 'L', 'unit' => NULL, 'unit_shortcut' => NULL],
+      ['value' => 'S', 'unit'  => NULL, 'unit_shortcut' => NULL],
+      ['value' => 'M', 'unit'  => NULL, 'unit_shortcut' => NULL],
+      ['value' => 'L', 'unit'  => NULL, 'unit_shortcut' => NULL],
       ['value' => 'XL', 'unit' => NULL, 'unit_shortcut' => NULL],
     ];
-    for ($i = 20 ; $i < 40 ; $i += 0.5) {
+    for ($i = 20 ; $i < 35 ; $i += 0.5) {
       $data[] = [
-        'value' => round($i, 1),
-        'unit' => 'centimeter',
+        'value'         => round($i, 1),
+        'unit'          => 'centimeter',
         'unit_shortcut' => 'cm',
       ];
-      // $data[] = [
-      //   'value' => round($i * 0.393701, 1),
-      //   'unit' => 'inch',
-      //   'unit_shortcut' => 'in',
-      // ];
     }
     foreach ($data as $element) {
       $item = (new Size())
