104a06d5e685859b98d8d1c5257291cd790d526c
Mickael MLP. LAO-PANE <mlao-pane@ciage.net>
2017-06-22 11:23:36 +0200

    Modified : TestController products generation (size matches category)

--- src/TrailWarehouse/AppBundle/Controller/Admin/TestController.php
+++ src/TrailWarehouse/AppBundle/Controller/Admin/TestController.php
@@ -26,14 +26,14 @@ class TestController extends Controller
         $families = $repository['family']->findAll();
 
         foreach ($families as $family) {
-          $item = (new Product())
+          $product = (new Product())
             ->setFamily($family)
             ->setColor($repository['color']->getOneRand(false))
-            ->setSize($repository['size']->getOneRand(false))
-            ->setPrice(mt_rand(2, 20) * 10)
-            ->setStock(mt_rand(1, 15) * 10)
+            ->setSize($repository['size']->getOneRandBy('category', $family->getCategory(), false))
+            ->setPrice($family->getCategory()->getName() == 'accessoires' ? mt_rand(1, 7) * 10 : mt_rand(2, 15) * 10)
+            ->setStock(mt_rand(0, 5) * 10)
           ;
-          $manager->persist($item);
+          $manager->persist($product);
         }
         $manager->flush();
         return $this->redirectToRoute('app_shop_categories', $data);
