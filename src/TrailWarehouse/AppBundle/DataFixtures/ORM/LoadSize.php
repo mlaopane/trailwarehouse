c0b1280e5e3fe8968554b3e152969d2f24831876
Mickael MLP. LAO-PANE <mlao-pane@ciage.net>
2017-06-22 11:35:08 +0200

    Fixed : Get the right Category entity instead of an array

--- src/TrailWarehouse/AppBundle/DataFixtures/ORM/LoadSize.php
+++ src/TrailWarehouse/AppBundle/DataFixtures/ORM/LoadSize.php
@@ -15,7 +15,7 @@ class LoadSize implements FixtureInterface, OrderedFixtureInterface
   {
     $repository['category'] = $manager->getRepository('TrailWarehouseAppBundle:Category');
 
-    $category = $repository['category']->findByName('textile');
+    $category = $repository['category']->findOneByName('textile');
     $data = [
       ['value' => 'U', 'unit'  => NULL, 'unit_shortcut' => NULL, 'category' => $category],
       ['value' => 'XS', 'unit' => NULL, 'unit_shortcut' => NULL, 'category' => $category],
@@ -25,7 +25,7 @@ class LoadSize implements FixtureInterface, OrderedFixtureInterface
       ['value' => 'XL', 'unit' => NULL, 'unit_shortcut' => NULL, 'category' => $category],
     ];
 
-    $category = $repository['category']->findByName('chaussures');
+    $category = $repository['category']->findOneByName('chaussures');
     for ($i = 20 ; $i < 35 ; $i += 0.5) {
       $data[] = [
         'value'         => round($i, 1),
