f63c259e46176ffd42768c23894adacd76c275e5
mlaopane <mlaopane@gmail.com>
2017-07-22 15:21:48 +0200

    Installed : KnpSnappyBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -26,6 +26,7 @@ class AppKernel extends Kernel
             new TrailWarehouse\UserBundle\TrailWarehouseUserBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
+            new Knp\Bundle\SnappyBundle\KnpSnappyBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
