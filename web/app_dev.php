579edfc0a044ca52e8113f814d65033440b6df97
mlaopane <mlaopane@gmail.com>
2017-07-27 03:45:02 +0200

    Fixed : easy_admin config by adding a '%' to escape the 'brand_color' value Updated : app_dev.php to allow external access

--- web/app_dev.php
+++ web/app_dev.php
@@ -12,7 +12,7 @@ use Symfony\Component\Debug\Debug;
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1', '176.146.60.80'])
+    || !(in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1', '176.146.60.80'])
     || php_sapi_name() === 'cli-server')
 ) {
     header('HTTP/1.0 403 Forbidden');
