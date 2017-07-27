5d63b97bc74ad9d99f96a2bfd2ffd16b6bf4ea3d
mlaopane <mlaopane@gmail.com>
2017-07-27 22:13:58 +0200

    Update : Home view (not done) + style + category view

--- src/TrailWarehouse/AppBundle/Controller/HomeController.php
+++ src/TrailWarehouse/AppBundle/Controller/HomeController.php
@@ -20,15 +20,15 @@ class HomeController extends Controller
 
   public function indexAction(EntityManagerInterface $em)
   {
+    return $this->redirectToRoute('app_shop_categories');
     $data['brands'] = $this->repo['brand']->findAll(['brand' => 'asc']);
     $data['categories'] = $this->repo['category']->findAll(['category' => 'asc']);
     $data['selections'] = [];
+
     foreach ($data['categories'] as $category)
     {
       $data['selections'][$category->getSlug()] = $this->repo['family']->getBestOfCategory($category, 3, false);
     }
-
-    dump($data['selections']);
 
     return $this->render('TrailWarehouseAppBundle:Home:index.html.twig', $data);
   }
