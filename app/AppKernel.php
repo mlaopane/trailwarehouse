2f21061dd65f4b933904d10bc53927cf3703a9b9
mlaopane <mlaopane@gmail.com>
2017-07-27 06:00:57 +0200

    Removed : ConsoleBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -36,7 +36,6 @@ class AppKernel extends Kernel
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
             $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
             $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
-            $bundles[] = new CoreSphere\ConsoleBundle\CoreSphereConsoleBundle();
         }
 
         return $bundles;
