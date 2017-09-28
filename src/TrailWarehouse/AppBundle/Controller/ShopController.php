d5168bc3933bf8e61ef486628498772158a97dab
mlaopane <mlaopane@gmail.com>
2017-09-28 14:08:23 +0200

    Added : - RepositoryManager service - ShopController using Repository Manager + private methods added for initialization

--- src/TrailWarehouse/AppBundle/Controller/ShopController.php
+++ src/TrailWarehouse/AppBundle/Controller/ShopController.php
@@ -18,107 +18,119 @@ use TrailWarehouse\AppBundle\Entity\Category;
 use TrailWarehouse\AppBundle\Form\CartType;
 use TrailWarehouse\AppBundle\Form\PromoType;
 use TrailWarehouse\AppBundle\Controller\CartController;
+use TrailWarehouse\AppBundle\Service\RepositoryManager;
 use Doctrine\ORM\EntityManagerInterface;
 
 class ShopController extends Controller
 {
+    private $repo;
+    private $serializer;
 
-  protected $repo;
-  protected $serializer;
+    public function __construct(SessionInterface $session, RepositoryManager $rm)
+    {
+        $this->repo = [
+            'brand'    => $rm->get('Brand'),
+            'category' => $rm->get('Category'),
+            'family'   => $rm->get('Family'),
+            'product'  => $rm->get('Product'),
+            'vat'      => $rm->get('Vat'),
+        ];
 
-  public function __construct(SessionInterface $session, EntityManagerInterface $em)
-  {
-    $this->repo = [
-      'brand'    => $em->getRepository('TrailWarehouseAppBundle:Brand'),
-      'category' => $em->getRepository('TrailWarehouseAppBundle:Category'),
-      'family'   => $em->getRepository('TrailWarehouseAppBundle:Family'),
-      'product'  => $em->getRepository('TrailWarehouseAppBundle:Product'),
-      'vat'      => $em->getRepository('TrailWarehouseAppBundle:Vat'),
-    ];
-
-    $normalizer = new ObjectNormalizer();
-    $normalizer->setCircularReferenceHandler(function ($object) {
-      return $object->getId();
-    });
-    $normalizers = [ $normalizer ];
-    $encoders    = [ new JsonEncoder() ];
-    $this->serializer = new Serializer($normalizers, $encoders);
-
-    if (empty($cart = $session->get('cart'))) {
-      $vat = $this->repo['vat']->findOneByCountry('fr');
-      $session->set('cart', (new Cart())->setVat($vat));
-    }
-  }
-
-  /**
-   * 'app_shop_search'
-   */
-  public function searchAction()
-  {
-    $data = [
-      'families' => [],
-    ];
-    return $this->render('TrailWarehouseAppBundle:Shop:search.html.twig', $data);
-  }
-
-  /**
-   * Gets the categories then render the menu
-   */
-  public function menuAction($active_category = NULL)
-  {
-    $data = [
-      'categories'      => $this->repo['category']->findAll(['category' => 'asc']),
-      'active_category' => $active_category,
-    ];
-    return $this->render('TrailWarehouseAppBundle:Shop:menu.html.twig', $data);
-  }
-
-  /**
-   * 'app_shop_categories'
-   */
-  public function categoriesAction()
-  {
-    $data = [
-      'active_category' => ['name' => 'toutes'],
-      'families'        => $this->repo['family']->findAll(),
-    ];
-
-    return $this->render('TrailWarehouseAppBundle:Shop:category.html.twig', $data);
-  }
-
-  /**
-   * 'app_shop_category'
-   *
-   * @ParamConverter("category", options={"mapping": {"slug": "slug"}})
-   */
-  public function categoryAction(Category $category, string $slug, EntityManagerInterface $em)
-  {
-    $data = [
-      'active_category' => $category,
-      'families'        => $this->repo['family']->findByCategory($category),
-    ];
-
-    return $this->render('TrailWarehouseAppBundle:Shop:category.html.twig', $data);
-  }
-
-  /**
-   * 'app_shop_family'
-   * @param Family $family (from slug)
-   *
-   */
-  public function familyAction(Family $family, EntityManagerInterface $em)
-  {
-    if ($family->getProducts()->count() == 0) {
-      return $this->redirectToRoute('app_home');
+        $this->initSerializer()->initSession($session);
     }
 
-    $data = [
-      'family' => $family,
-      'colors' => $this->repo['product']->getColorsByFamily($family),
-      'sizes'  => $this->repo['product']->getSizesByFamily($family),
-    ];
+    private function initSerializer()
+    {
+        $normalizer = new ObjectNormalizer();
+        $normalizer->setCircularReferenceHandler(function ($object) {
+            return $object->getId();
+        });
+        $normalizers = [ $normalizer ];
+        $encoders    = [ new JsonEncoder() ];
+        $this->serializer = new Serializer($normalizers, $encoders);
 
-    return $this->render('TrailWarehouseAppBundle:Shop:family.html.twig', $data);
-  }
+        return $this;
+    }
 
+    private function initSession(SessionInterface $session)
+    {
+        if (empty($cart = $session->get('cart'))) {
+            $vat = $this->repo['vat']->findOneByCountry('fr');
+            $session->set('cart', (new Cart())->setVat($vat));
+        }
+
+        return $this;
+    }
+
+    /**
+     * 'app_shop_search'
+     */
+    public function searchAction()
+    {
+        $data = [
+            'families' => [],
+        ];
+
+        return $this->render('TrailWarehouseAppBundle:Shop:search.html.twig', $data);
+    }
+
+    /**
+     * Gets the categories then render the menu
+     */
+    public function menuAction($active_category = null)
+    {
+        $data = [
+            'categories'      => $this->repo['category']->findAll(['category' => 'asc']),
+            'active_category' => $active_category,
+        ];
+        return $this->render('TrailWarehouseAppBundle:Shop:menu.html.twig', $data);
+    }
+
+    /**
+     * 'app_shop_categories'
+     */
+    public function categoriesAction()
+    {
+        $data = [
+            'active_category' => ['name' => 'toutes'],
+            'families'        => $this->repo['family']->findAll(),
+        ];
+
+        return $this->render('TrailWarehouseAppBundle:Shop:category.html.twig', $data);
+    }
+
+    /**
+     * 'app_shop_category'
+     *
+     * @ParamConverter("category", options={"mapping": {"slug": "slug"}})
+    */
+    public function categoryAction(Category $category, string $slug, EntityManagerInterface $em)
+    {
+        $data = [
+            'active_category' => $category,
+            'families'        => $this->repo['family']->findByCategory($category),
+        ];
+
+        return $this->render('TrailWarehouseAppBundle:Shop:category.html.twig', $data);
+    }
+
+    /**
+    * 'app_shop_family'
+    * @param Family $family (from slug)
+    *
+    */
+    public function familyAction(Family $family, EntityManagerInterface $em)
+    {
+        if ($family->getProducts()->count() == 0) {
+            return $this->redirectToRoute('app_home');
+        }
+
+        $data = [
+            'family' => $family,
+            'colors' => $this->repo['product']->getColorsByFamily($family),
+            'sizes'  => $this->repo['product']->getSizesByFamily($family),
+        ];
+
+        return $this->render('TrailWarehouseAppBundle:Shop:family.html.twig', $data);
+    }
 }
---
+++ src/TrailWarehouse/AppBundle/Service/RepositoryManager.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace TrailWarehouse\AppBundle\Service;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+/**
+ *
+ *
+ * @author Mickaël LAO-PANE
+ */
+class RepositoryManager
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    protected $em;
+
+    /**
+     *
+     */
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    /**
+     * @param string entityName (ucfirst + lcase)
+     * @return EntityRepository
+     */
+    public function get(string $entityName): EntityRepository
+    {
+        return $this->em->getRepository('TrailWarehouseAppBundle:' . $entityName);
+    }
+}
